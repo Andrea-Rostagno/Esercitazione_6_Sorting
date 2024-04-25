bea0f64bcacf27608a0ee41ee6a24cfef999f5ad
Andrea-Rostagno <s295706@studenti.polito.it>
2024-04-25 12:44:55 +0200

    ok

--- Esercizio_2/main.cpp
+++ Esercizio_2/main.cpp
@@ -4,53 +4,56 @@
 #include<vector>
 #include<numeric>
 #include<algorithm>
+#include <random>
 #include"src/SortingAlgorithm.hpp"
-
 
 using namespace std;
 using namespace chrono;
 
+int BubbleTime(vector<int> v){
+    steady_clock::time_point t_begin = steady_clock::now();
+    _sleep(1);
+    SortLibrary::BubbleSort(v);
+    steady_clock::time_point t_end = steady_clock::now();
+    double duration = duration_cast<milliseconds>(t_end - t_begin).count();
+    //cout<<"la durata ammonta a: "<<duration<<" millisecondi"<<endl;
+    return duration;
+}
+
+int MergeTime(vector<int> v){
+    steady_clock::time_point t_begin = steady_clock::now();
+    _sleep(1);
+    SortLibrary::MergeSort(v);
+    steady_clock::time_point t_end = steady_clock::now();
+    double duration = duration_cast<milliseconds>(t_end - t_begin).count();
+    //cout<<"la durata ammonta a: "<<duration<<" millisecondi"<<endl;
+    return duration;
+}
+
 int main()
 {
+    double Somma1=0;
+    double Somma2=0;
     int lunghezza=10;
-    cout<<"lunghezza "<<lunghezza<<endl;
-    vector<int> v;
-    int a=0;
-    for (int i = 0; i < lunghezza; ++i) {
-        a=rand()%100;
-        v.push_back(a);
-        cout<<v[i]<<" ";
-
+    int n=5;
+    cout<<"Lunghezza vettore "<<lunghezza<<endl;
+    vector<int> v(lunghezza);
+    for (int i = 0; i < n; ++i) {
+        mt19937 rng(random_device{}());
+        uniform_int_distribution<int> dist(1, 100);
+        cout<<"Vettore casuale "<<i+1<<": ";
+        for (int i = 0; i < lunghezza; ++i) {
+            v[i]=dist(rng);
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        Somma1=Somma1+BubbleTime(v);
+        Somma2=Somma2+MergeTime(v);
     }
-    cout<<endl;
-
-    std::chrono::steady_clock::time_point t_begin = std::chrono::steady_clock::now();
-    _sleep(1);
-    //SortLibrary::BubbleSort(v);
-    SortLibrary::MergeSort(v);
-    std::chrono::steady_clock::time_point t_end = std::chrono::steady_clock::now();
-    double duration = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_begin).count();
-    cout<<"la durata e "<<duration<<endl;
-    cout<<"v nuovo ";
-    for (int i = 0; i < lunghezza; ++i) {
-        cout<<v[i]<<" ";
-
-    }
-    //SortLibrary::MergeSort(v);
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cout<<"La media dei tempi di BubbleSort: "<<Somma1/n<<" millisecondi"<<endl;
+    cout<<"La media dei tempi di MergeSort: "<<Somma2/n<<" millisecondi"<<endl;
 
     return 0;
 }
+
 
